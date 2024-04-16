6fb83916536a981b2277e6175077536d8fd9e9a7
hyun7520 <sanghyun123452@gmail.com>
2024-04-16 17:18:15 +0900

    프로젝트 기초 세팅

---
+++ src/main/java/com/daedongmap/daedongmap/DaedongmapApplication.java
@@ -0,0 +1,13 @@
+package com.daedongmap.daedongmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaedongmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaedongmapApplication.class, args);
+	}
+
+}
