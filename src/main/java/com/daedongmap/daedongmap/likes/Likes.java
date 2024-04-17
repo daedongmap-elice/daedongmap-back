7801ff6bc1029469456d4d4831b8b77a1ca45c3f
Hajin74 <yoonmij9974@naver.com>
2024-04-17 17:22:08 +0900

    feat: Likes 엔티티

---
+++ src/main/java/com/daedongmap/daedongmap/likes/Likes.java
@@ -0,0 +1,22 @@
+package com.daedongmap.daedongmap.likes;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Likes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name="user_id")
+    @ManyToOne
+    private User user;
+
+    @JoinColumn(name="review_id")
+    @ManyToOne
+    private Review review;
+
+}
