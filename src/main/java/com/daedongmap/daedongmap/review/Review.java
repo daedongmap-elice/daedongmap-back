efe9df4e9d1b150e51a0d4bbd0e8587efd36c549
Hajin74 <yoonmij9974@naver.com>
2024-04-17 15:57:52 +0900

    feat: Review 엔티티

---
+++ src/main/java/com/daedongmap/daedongmap/review/Review.java
@@ -0,0 +1,43 @@
+package com.daedongmap.daedongmap.review;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private User user;
+
+    private Place place;
+
+    private String title;
+
+    private String content;
+
+    private int rating;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
