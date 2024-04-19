ea0ac2fb6cdf61f5d6db539b949086302899d73a
hyun7520 <sanghyun123452@gmail.com>
2024-04-19 19:02:05 +0900

    Fix: 유저 수정 기능 API만 작성, 문법 수정

--- src/main/java/com/daedongmap/daedongmap/user/service/UserService.java
+++ src/main/java/com/daedongmap/daedongmap/user/service/UserService.java
@@ -7,6 +7,7 @@ import com.daedongmap.daedongmap.user.domain.Users;
 import com.daedongmap.daedongmap.user.dto.AuthResponse;
 import com.daedongmap.daedongmap.user.dto.UserLoginDto;
 import com.daedongmap.daedongmap.user.dto.UserRegisterDto;
+import com.daedongmap.daedongmap.user.dto.UserUpdateDto;
 import com.daedongmap.daedongmap.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -26,11 +27,9 @@ public class UserService {
     @Transactional
     public AuthResponse registerUser(UserRegisterDto userRegisterDto) {
 
-        Optional<Users> foundUser = userRepository.findByEmail(userRegisterDto.getEmail());
-
-        if(foundUser.isPresent()) {
+        userRepository.findByEmail(userRegisterDto.getEmail()).ifPresent(e -> {
             throw new CustomException(ErrorCode.EMAIL_IN_USE);
-        }
+        });
 
         Users newUsers = Users.builder()
                 .nickName(userRegisterDto.getNickName())
@@ -43,19 +42,22 @@ public class UserService {
     }
 
     public AuthResponse loginUser(UserLoginDto userLoginDto) {
-        Optional<Users> foundUser = userRepository.findByEmail(userLoginDto.getEmail());
 
-        if(foundUser.isEmpty()) {
-            throw new CustomException(ErrorCode.USER_NOT_FOUND);
-        }
-
-        Users users = foundUser.get();
+        Users foundUser = userRepository.findByEmail(userLoginDto.getEmail())
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
         
         // matches(입력된 비밀번호, 암호화된 비밀번호)
-        if(!passwordEncoder.matches(userLoginDto.getPassword(), users.getPassword())) {
+        if(!passwordEncoder.matches(userLoginDto.getPassword(), foundUser.getPassword())) {
             throw new CustomException(ErrorCode.INVALID_PASSWORD);
         }
 
-        return new AuthResponse(users.getNickName(), tokenProvider.createToken(users));
+        return new AuthResponse(foundUser.getNickName(), tokenProvider.createToken(foundUser));
+    }
+
+    public Users updateUser(UserUpdateDto userUpdateDto) {
+        Users foundUser = userRepository.findByEmail(userUpdateDto.getEmail())
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        return foundUser;
     }
 }
