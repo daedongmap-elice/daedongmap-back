aba50b43ecdbfa717bf8568157358d04c5a8cb2e
Hajin74 <yoonmij9974@naver.com>
2024-04-23 00:50:15 +0900

    refactor: entity -> dto 변환 작업 위치 변경(service -> dto)

--- src/main/java/com/daedongmap/daedongmap/review/dto/ReviewBasicInfoDto.java
+++ src/main/java/com/daedongmap/daedongmap/review/dto/ReviewBasicInfoDto.java
@@ -1,5 +1,6 @@
 package com.daedongmap.daedongmap.review.dto;
 
+import com.daedongmap.daedongmap.review.domain.Review;
 import com.daedongmap.daedongmap.user.dto.UserBasicInfoDto;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,8 +10,8 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@Builder
 public class ReviewBasicInfoDto {
+
     private Long id;
     private Long placeId;
     private UserBasicInfoDto user;
@@ -22,5 +23,19 @@ public class ReviewBasicInfoDto {
     private float averageRating;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public ReviewBasicInfoDto(Review review) {
+        this.id = review.getId();
+        this.placeId = review.getPlace().getId();
+        this.user = new UserBasicInfoDto(review.getUser());
+        this.title = review.getTitle();
+        this.content = review.getContent();
+        this.hygieneRating = review.getHygieneRating();
+        this.tasteRating = review.getTasteRating();
+        this.kindnessRating = review.getKindnessRating();
+        this.averageRating = review.getAverageRating();
+        this.createdAt = review.getCreatedAt();
+        this.updatedAt = review.getUpdatedAt();
+    }
 
 }
