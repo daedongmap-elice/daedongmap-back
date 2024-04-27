9ae7d5370b6477590d9f15a8c10d7f25b8ab8792
Hajin74 <yoonmij9974@naver.com>
2024-04-27 23:58:58 +0900

    feat: 원댓글 삭제시, 달려있는 대댓글도 모두 삭제

--- src/main/java/com/daedongmap/daedongmap/comment/service/CommentService.java
+++ src/main/java/com/daedongmap/daedongmap/comment/service/CommentService.java
@@ -18,10 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -84,6 +81,10 @@ public class CommentService {
 
     @Transactional
     public void deleteComment(Long commentId) {
+        List<Comment> replies = commentRepository.findAllByParentId(commentId);
+        for (Comment reply : replies) {
+            commentRepository.deleteById(reply.getId());
+        }
         commentRepository.deleteById(commentId);
     }
 
