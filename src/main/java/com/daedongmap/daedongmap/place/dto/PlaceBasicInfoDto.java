57cc2675346212d955cb843bb048121bc5bee8ad
ldg <dadk93@gmail.com>
2024-04-29 14:04:38 +0900

    dk: 240428 JsonNaming 삭제

--- src/main/java/com/daedongmap/daedongmap/place/dto/PlaceBasicInfoDto.java
+++ src/main/java/com/daedongmap/daedongmap/place/dto/PlaceBasicInfoDto.java
@@ -1,7 +1,6 @@
 package com.daedongmap.daedongmap.place.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +9,6 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @Builder
-@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class PlaceBasicInfoDto {
     private Long id;
     private String kakaoPlaceId;
