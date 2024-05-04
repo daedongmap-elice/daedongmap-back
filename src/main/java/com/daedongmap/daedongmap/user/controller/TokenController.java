b5ac666acb244bf3a85577f69311115c5be28adf
hyun7520 <sanghyun123452@gmail.com>
2024-05-05 08:13:40 +0900

    Fix: 다른 컨트롤러 facade 적용

--- src/main/java/com/daedongmap/daedongmap/user/controller/TokenController.java
+++ src/main/java/com/daedongmap/daedongmap/user/controller/TokenController.java
@@ -1,11 +1,7 @@
 package com.daedongmap.daedongmap.user.controller;
 
-import com.daedongmap.daedongmap.security.jwt.TokenProvider;
-import com.daedongmap.daedongmap.user.domain.Users;
 import com.daedongmap.daedongmap.user.dto.response.JwtTokenDto;
-import com.daedongmap.daedongmap.user.service.TokenFacade;
-import com.daedongmap.daedongmap.user.service.TokenService;
-import com.daedongmap.daedongmap.user.service.UserService;
+import com.daedongmap.daedongmap.user.service.Facade;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -20,14 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TokenController {
 
-    private final TokenFacade tokenFacade;
+    private final Facade facade;
 
     @GetMapping("/refresh-token")
     @Operation(summary = "access 토큰 새로 발급", description = "기존의 access 토큰 기한이 지난 경우 refresh token 전송받아 인증 절차 후 새로운 access token 발급")
     public ResponseEntity<JwtTokenDto> refreshToken(HttpServletRequest request) {
 
         String refreshToken = request.getHeader("Authorization");
-        JwtTokenDto newAccessToken = tokenFacade.getJwtTokenDto(refreshToken);
+        JwtTokenDto newAccessToken = facade.getJwtTokenDto(refreshToken);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
--- src/main/java/com/daedongmap/daedongmap/user/service/Facade.java
+++ src/main/java/com/daedongmap/daedongmap/user/service/Facade.java
@@ -3,20 +3,30 @@ package com.daedongmap.daedongmap.user.service;
 import com.daedongmap.daedongmap.security.jwt.TokenProvider;
 import com.daedongmap.daedongmap.user.domain.Users;
 import com.daedongmap.daedongmap.user.dto.response.JwtTokenDto;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class TokenFacade {
+public class Facade {
 
     private final TokenService tokenService;
     private final UserService userService;
     private final TokenProvider tokenProvider;
 
+    @Transactional
     public JwtTokenDto getJwtTokenDto(String refreshToken) {
         Users foundUser = userService.findUserById(tokenService.validate(refreshToken));
 
         return tokenProvider.createNewAccessToken(foundUser, foundUser.getRoles());
+    }
+
+    @Transactional
+    public String logoutUser(String refreshToken) {
+
+        Long userId = tokenService.validate(refreshToken);
+
+        return tokenService.deleteRefreshByUserId(userId);
     }
 }
