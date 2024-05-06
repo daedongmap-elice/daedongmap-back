029386ef1b454fe2a9a225440a838dec6e9937dc
Hajin74 <yoonmij9974@naver.com>
2024-05-06 17:40:32 +0900

    feat: 이미지 통신 테스트 api

--- src/main/java/com/daedongmap/daedongmap/review/controller/ReviewController.java
+++ src/main/java/com/daedongmap/daedongmap/review/controller/ReviewController.java
@@ -6,7 +6,11 @@ import com.daedongmap.daedongmap.place.dto.PlaceCreateDto;
 import com.daedongmap.daedongmap.place.service.PlaceService;
 import com.daedongmap.daedongmap.review.dto.*;
 import com.daedongmap.daedongmap.review.domain.Review;
+import com.daedongmap.daedongmap.review.repository.ReviewRepository;
 import com.daedongmap.daedongmap.review.service.ReviewService;
+import com.daedongmap.daedongmap.reviewImage.domain.ReviewImage;
+import com.daedongmap.daedongmap.reviewImage.repository.ReviewImageRepository;
+import com.daedongmap.daedongmap.reviewImage.service.ReviewImageService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -26,14 +31,32 @@ public class ReviewController {
 
     private final ReviewService reviewService;
     private final PlaceService placeService;
+    private final ReviewImageService reviewImageService;
+    private final ReviewImageRepository reviewImageRepository;
 
     @PostMapping("/api/reviews")
     @Operation(summary = "리뷰 작성", description = "리뷰를 작성합니다.")
     public ResponseEntity<ReviewDto> createReview(@RequestPart(value="file") List<MultipartFile> multipartFileList,
                                                   @RequestPart(value="reviewRequest") ReviewCreateDto reviewCreateDto,
-                                                  @RequestPart(value="placeRequest")PlaceCreateDto placeCreateDto) throws IOException {
+                                                  @RequestPart(value="placeRequest") PlaceCreateDto placeCreateDto) throws IOException {
         ReviewDto createdReviewDto = reviewService.createReview(multipartFileList, reviewCreateDto, placeCreateDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdReviewDto);
+    }
+
+    // 이미지 업로드 확인용 api
+    @PostMapping("/api/reviews/test")
+    @Operation(summary = "테스트", description = "테스트")
+    public ResponseEntity<ReviewImage> reviewTest(@RequestPart(value="file") MultipartFile multipartFile) throws IOException {
+        String fileName = multipartFile.getOriginalFilename() + "_" + UUID.randomUUID();
+        String filePath = reviewImageService.uploadReviewImage(multipartFile, fileName);
+
+        ReviewImage reviewImage = ReviewImage.builder()
+                .filePath(filePath)
+                .fileName(fileName)
+                .build();
+
+        reviewImageRepository.save(reviewImage);
+        return ResponseEntity.status(HttpStatus.CREATED).body(reviewImage);
     }
 
     @GetMapping("/api/reviews")
