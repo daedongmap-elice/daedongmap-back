0242f56765ce50aad8fe89aa08d90f4c79201a22
Hajin74 <yoonmij9974@naver.com>
2024-05-08 16:56:43 +0900

    feat: 팔로우/언팔로우 조건 추가

--- src/main/java/com/daedongmap/daedongmap/follow/controller/FollowController.java
+++ src/main/java/com/daedongmap/daedongmap/follow/controller/FollowController.java
@@ -3,11 +3,13 @@ package com.daedongmap.daedongmap.follow.controller;
 import com.daedongmap.daedongmap.follow.dto.FollowerDto;
 import com.daedongmap.daedongmap.follow.dto.FollowingDto;
 import com.daedongmap.daedongmap.follow.service.FollowService;
+import com.daedongmap.daedongmap.user.domain.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,14 +23,22 @@ public class FollowController {
 
     @PostMapping("/api/follows")
     @Operation(summary = "팔로우하기", description = "상대방을 팔로우 합니다.")
-    public ResponseEntity<?> doFollow(@RequestParam Long followerId, @RequestParam Long followingId) {
+    public ResponseEntity<?> doFollow(@AuthenticationPrincipal CustomUserDetails tokenUser,
+                                      @RequestParam Long followingId) {
+        Long followerId = tokenUser.getUser().getId();
+        log.info("팔로우하기 api 호출 - 팔로잉 상대 : " + followingId);
+
         followService.doFollow(followerId, followingId);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/api/follows")
     @Operation(summary = "팔로우 취소하기", description = "상대방을 팔로우 취소합니다.")
-    public ResponseEntity<?> unFollow(@RequestParam Long followerId, @RequestParam Long followingId) {
+    public ResponseEntity<?> unFollow(@AuthenticationPrincipal CustomUserDetails tokenUser,
+                                      @RequestParam Long followingId) {
+        Long followerId = tokenUser.getUser().getId();
+        log.info("팔로우 취소 api 호출 - 팔로잉 취소 상대 : " + followingId);
+
         followService.unFollow(followerId, followingId);
         return ResponseEntity.ok().build();
     }
