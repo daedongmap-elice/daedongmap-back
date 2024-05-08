e2b2fb223277949b7debf93aa6fc5f7e40625802
hyun7520 <sanghyun123452@gmail.com>
2024-05-08 14:34:18 +0900

    Fix: 불필요한 인터페이스 삭제

--- src/main/java/com/daedongmap/daedongmap/user/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.daedongmap.daedongmap.user.repository;
-
-import com.daedongmap.daedongmap.user.domain.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-}
--- src/main/java/com/daedongmap/daedongmap/user/service/UserService.java
+++ src/main/java/com/daedongmap/daedongmap/user/service/UserService.java
@@ -13,7 +13,6 @@ import com.daedongmap.daedongmap.user.dto.request.UserUpdateDto;
 import com.daedongmap.daedongmap.user.dto.response.AuthResponseDto;
 import com.daedongmap.daedongmap.user.dto.response.JwtTokenDto;
 import com.daedongmap.daedongmap.user.dto.response.UserResponseDto;
-import com.daedongmap.daedongmap.user.repository.AuthorityRepository;
 import com.daedongmap.daedongmap.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -33,7 +32,6 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final AuthorityRepository authorityRepository;
     private final BCryptPasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
     private final AmazonS3Client amazonS3Client;
