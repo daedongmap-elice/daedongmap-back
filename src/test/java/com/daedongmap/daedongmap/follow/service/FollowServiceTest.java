cea46330ff38a7d17ef248c1aac5e77274d669b0
Hajin74 <yoonmij9974@naver.com>
2024-05-24 13:28:29 +0900

    feat: 팔로잉 & 팔로워 리스트 조회 - 테스트 코드

--- src/test/java/com/daedongmap/daedongmap/follow/service/FollowServiceTest.java
+++ src/test/java/com/daedongmap/daedongmap/follow/service/FollowServiceTest.java
@@ -3,6 +3,8 @@ package com.daedongmap.daedongmap.follow.service;
 import com.daedongmap.daedongmap.alarm.service.AlarmService;
 import com.daedongmap.daedongmap.exception.CustomException;
 import com.daedongmap.daedongmap.exception.ErrorCode;
+import com.daedongmap.daedongmap.follow.dto.FollowerDto;
+import com.daedongmap.daedongmap.follow.dto.FollowingDto;
 import com.daedongmap.daedongmap.follow.model.Follow;
 import com.daedongmap.daedongmap.follow.repository.FollowRepository;
 import com.daedongmap.daedongmap.likes.repository.LikeRepository;
@@ -20,7 +22,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -209,6 +213,110 @@ class FollowServiceTest {
         // then
         verify(followRepository, never()).delete(any(Follow.class));
         assertEquals(ErrorCode.UNFOLLOW_MYSELF_NOW_ALLOWED, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("팔로잉 리스트 조회 (성공)")
+    void getFollowingList_success() {
+        // given
+        // mockFollower가 mockUser1과 mockUser2를 팔로잉함
+        Users mockUser1 = Users.builder()
+                .id(1L)
+                .nickName("mockUser1")
+                .isMember(true)
+                .role(Collections.singletonList(Authority.builder().role("ROLE_USER").build()))
+                .build();
+
+        Users mockUser2 = Users.builder()
+                .id(2L)
+                .nickName("mockUser2")
+                .isMember(true)
+                .role(Collections.singletonList(Authority.builder().role("ROLE_USER").build()))
+                .build();
+
+        Users mockFollower = Users.builder()
+                .id(3L)
+                .nickName("mockFollower")
+                .isMember(true)
+                .role(Collections.singletonList(Authority.builder().role("ROLE_USER").build()))
+                .build();
+
+        Follow mockFollow1 = new Follow(mockFollower, mockUser1);
+        Follow mockFollow2 = new Follow(mockFollower, mockUser2);
+
+        List<Follow> mockFollowingList = new ArrayList<>();
+        mockFollowingList.add(mockFollow1);
+        mockFollowingList.add(mockFollow2);
+
+        lenient().when(userRepository.findById(mockUser1.getId())).thenReturn(Optional.of(mockUser1));
+        lenient().when(userRepository.findById(mockUser2.getId())).thenReturn(Optional.of(mockUser1));
+        lenient().when(userRepository.findById(mockFollower.getId())).thenReturn(Optional.of(mockFollower));
+
+        lenient().when(followRepository.findByFollowerAndFollowing(mockFollower, mockUser1)).thenReturn(mockFollow1);
+        lenient().when(followRepository.findByFollowerAndFollowing(mockFollower, mockUser2)).thenReturn(mockFollow2);
+        lenient().when(followRepository.findAllByFollower(mockFollower)).thenReturn(mockFollowingList);
+
+        // when
+        List<FollowingDto> followingDtoList = followService.getFollowingList(mockFollower.getId());
+
+        // then
+        assertNotNull(followingDtoList);
+        assertEquals(mockFollowingList.size(), followingDtoList.size());
+        for (int i = 0; i < mockFollowingList.size(); i++) {
+            assertEquals(mockFollowingList.get(i).getFollowing().getNickName(), followingDtoList.get(i).getFollowing().getNickName());
+        }
+    }
+
+    @Test
+    @DisplayName("팔로워 리스트 조회 (성공)")
+    void getFollowerList_success() {
+        // given
+        // mockFollower1과 mockFollower2가 mockUser의 팔로워임
+        Users mockFollower1 = Users.builder()
+                .id(1L)
+                .nickName("mockFollower1")
+                .isMember(true)
+                .role(Collections.singletonList(Authority.builder().role("ROLE_USER").build()))
+                .build();
+
+        Users mockFollower2 = Users.builder()
+                .id(2L)
+                .nickName("mockFollower2")
+                .isMember(true)
+                .role(Collections.singletonList(Authority.builder().role("ROLE_USER").build()))
+                .build();
+
+        Users mockUser = Users.builder()
+                .id(3L)
+                .nickName("mockUser")
+                .isMember(true)
+                .role(Collections.singletonList(Authority.builder().role("ROLE_USER").build()))
+                .build();
+
+        Follow mockFollow1 = new Follow(mockFollower1, mockUser);
+        Follow mockFollow2 = new Follow(mockFollower2, mockUser);
+
+        List<Follow> mockFollowingList = new ArrayList<>();
+        mockFollowingList.add(mockFollow1);
+        mockFollowingList.add(mockFollow2);
+
+        lenient().when(userRepository.findById(mockFollower1.getId())).thenReturn(Optional.of(mockFollower1));
+        lenient().when(userRepository.findById(mockFollower2.getId())).thenReturn(Optional.of(mockFollower2));
+        lenient().when(userRepository.findById(mockUser.getId())).thenReturn(Optional.of(mockUser));
+
+        lenient().when(followRepository.findByFollowerAndFollowing(mockFollower1, mockUser)).thenReturn(mockFollow1);
+        lenient().when(followRepository.findByFollowerAndFollowing(mockFollower2, mockUser)).thenReturn(mockFollow2);
+        lenient().when(followRepository.findAllByFollowing(mockUser)).thenReturn(mockFollowingList);
+
+        // when
+        List<FollowerDto> followerDtoList = followService.getFollowerList(mockUser.getId());
+
+        // then
+        assertNotNull(followerDtoList);
+        assertEquals(mockFollowingList.size(), followerDtoList.size());
+        for (int i = 0; i < mockFollowingList.size(); i++) {
+            assertEquals(mockFollowingList.get(i).getFollower().getNickName(), followerDtoList.get(i).getFollower().getNickName());
+        }
     }
 
 }
